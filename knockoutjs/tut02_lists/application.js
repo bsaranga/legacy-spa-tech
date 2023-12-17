67950eea9f9f567da641042fc2482f844a935d72
bsaranga <bsaranga@gmail.com>
2023-12-17 23:35:12 +0530

    other bindings and using computed properties to do formatting

--- knockoutjs/tut02_lists/application.js
+++ knockoutjs/tut02_lists/application.js
@@ -4,6 +4,11 @@ function SeatReservation(id, name, initialMeal) {
     self.id = id;
     self.name = name;
     self.meal = ko.observable(initialMeal);
+
+    self.formattedPrice = ko.computed(function() {
+        var price = self.meal().price;
+        return price ? "$" + price.toFixed(2) : "None";        
+    });
 }
 
 // Overall viewmodel for this screen, along with initial state
